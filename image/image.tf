ee0bc265ad7a0f7c9463b37cff05c08b5bb1c6cd
guswl0701 <guswl0701@naver.com>
2024-07-30 12:18:51 +0900

    12:18

--- image/image.tf
+++ image/image.tf
@@ -1,26 +1,42 @@
 # Django 앱 이미지 푸시
 resource "null_resource" "push_django_image" {
-  provisioner "local-exec" {
-    command = <<EOF
-      aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${aws_ecr_repository.django_app.repository_url}
-      docker pull wangamy/newtickettopia:latest
-      docker tag wangamy/newtickettopia:latest ${aws_ecr_repository.django_app.repository_url}:latest
-      docker push ${aws_ecr_repository.django_app.repository_url}:latest
-    EOF
+  triggers = {
+    always_run = "${timestamp()}"
   }
 
-  depends_on = [aws_ecr_repository.django_app]
+  provisioner "local-exec" {
+    command = <<EOF
+      aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${data.aws_ecr_repository.django_app.repository_url}
+      docker pull wangamy/newticket:latest
+      docker tag wangamy/newticket:latest ${data.aws_ecr_repository.django_app.repository_url}:latest
+      docker push ${data.aws_ecr_repository.django_app.repository_url}:latest
+    EOF
+  }
 }
 
 # ECS 서비스가 새 이미지를 사용하도록 강제 업데이트
 resource "null_resource" "force_ecs_deployment" {
-  provisioner "local-exec" {
-    command = "aws ecs update-service --cluster ${aws_ecs_cluster.main.name} --service ${aws_ecs_service.app.name} --force-new-deployment --region ap-northeast-2"
+  triggers = {
+    django_image_update = null_resource.push_django_image.id
   }
 
-  depends_on = [
-    null_resource.push_django_image,
-    null_resource.build_and_push_nginx_image,
-    aws_ecs_service.app
-  ]
+  provisioner "local-exec" {
+    command = "aws ecs update-service --cluster ${data.aws_ecs_cluster.main.cluster_name} --service ${data.aws_ecs_service.app.service_name} --force-new-deployment --region ap-northeast-2"
+  }
+
+  depends_on = [null_resource.push_django_image]
+}
+
+# 기존 리소스 참조를 위한 data 소스 추가
+data "aws_ecr_repository" "django_app" {
+  name = "django-app-repo"
+}
+
+data "aws_ecs_cluster" "main" {
+  cluster_name = "main-cluster"
+}
+
+data "aws_ecs_service" "app" {
+  cluster_arn = data.aws_ecs_cluster.main.arn
+  service_name = "app-service"
 }
